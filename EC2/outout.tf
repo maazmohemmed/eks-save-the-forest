77c01eb81f40aca315a0ef2a8a82e1b47c071dd4
maazmohemmed <maazashrra478@gmail.com>
2025-08-13 11:49:20 +0530

    Output correction

--- EC2/outout.tf
+++ EC2/outout.tf
@@ -1,7 +1,7 @@
 output "jenkins_public_ip" {
-  value = aws_instance.jenkins.public_ip
+  value = aws_instance.Jenkins_Server.public_ip
 }
 
 output "jenkins_url" {
-  value = "http://${aws_instance.jenkins.public_ip}:8080"
+  value = "http://${aws_instance.Jenkins_Server.public_ip}:8080"
 }
